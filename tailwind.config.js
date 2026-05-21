8ea8457c4b525f6c29ba15a73d251903a384c420
pixellabssupport-tech <pixellabs.support@gmail.com>
2026-05-21 06:08:49 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        orange: {
+          500: '#FF6B00',
+          600: '#E06000',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
